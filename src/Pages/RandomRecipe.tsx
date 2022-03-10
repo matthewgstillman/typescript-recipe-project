f0b565611ea58b5f45a6499f3f693c31ff038d90
matthewgstillman <matthewgstillman@gmail.com>
2022-03-09 18:26:09 -0700

    currently working

--- src/Pages/RandomRecipe.tsx
+++ src/Pages/RandomRecipe.tsx
@@ -25,24 +25,18 @@ const RandomRecipe: FC = () => {
     `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=1&tags=${cuisineType}`
   );
   const [randomRecipe, setRandomRecipe] = useState<Recipe[]>([
-    {
-      ingredientString: "",
-    },
+    { ingredientString: "" },
   ]);
   const [randomRecipeDataMeta, setRandomRecipeDataMeta] = useState<
     RandomMetaRecipe[]
   >([
     {
-      randomMetaTitle: "Dutch Babies With Mulberry-Rhubarb Compote",
-      randomMetaSourceUrl:
-        "http://www.foodista.com/recipe/F7B527KX/dutch-babies-with-mulberry-rhubarb-compote",
-      randomMetaReadyInMinutes: 45,
-      randomMetaImage:
-        "https://spoonacular.com/recipeImages/641766-556x370.jpg",
-      randomMetaSummary:
-        'Dutch Babies With Mulberry-Rhubarb Compote might be just the side dish you are searching for. One serving contains <b>429 calories</b>, <b>8g of protein</b>, and <b>16g of fat</b>. For <b>$2.58 per serving</b>, this recipe <b>covers 10%</b> of your daily requirements of vitamins and minerals. This recipe is liked by 34 foodies and cooks. It is a good option if you\'re following a <b>vegetarian</b> diet. From preparation to the plate, this recipe takes approximately <b>45 minutes</b>. Head to the store and pick up cup vanilla sugar, salt, orange juice, and a few other things to make it today. All things considered, we decided this recipe <b>deserves a spoonacular score of 35%</b>. This score is not so tremendous. Try <a href="https://spoonacular.com/recipes/mulberry-rhubarb-compote-80998">Mulberry-rhubarb Compote</a>, <a href="https://spoonacular.com/recipes/means-dutch-babies-141505">Mean\'s Dutch Babies</a>, and <a href="https://spoonacular.com/recipes/apricot-almond-dutch-babies-759970">Apricot-Almond Dutch Babies</a> for similar recipes.',
-      randomMetaInstructions:
-        "<ol><li>For the compote: Pulse the rhubarb in the food processor 5-6 times to a diced consistency. Then pulse the mulberries 3-4 times to chop.</li><li>Add rhubarb, mulberries, orange juice, sugar, salt and cornstarch to a sauce pan and simmer over medium for 10-15 minutesstirring occasionally.</li><li>Remove from heat and stir in the fresh mint leaves. Makes approximately 2 cups.</li><li>Dutch Babies: Preheat the oven to 375*F. In the blender, add the flour, sugar, salt, milk and eggs. Process for 10 seconds, then add 2 Tb. of melted butter and process again.</li><li>Brush the remaining 4 Tb. of butter into 24 muffin tinsthoroughly coating. Slowly pour the batter into the muffins tins.</li><li>Bake for 12-14 minutesuntil edges are golden brown and the center is puffed.</li><li>Immediately remove the Dutch babies from the muffins tins and top with mulberry-rhubarb compote!</li><li>Serves 6+.</li><li>If making the traditional way, this is enough batter for two cast-iron skillets.</li></ol>",
+      randomMetaTitle: "",
+      randomMetaSourceUrl: "",
+      randomMetaReadyInMinutes: "",
+      randomMetaImage: "",
+      randomMetaSummary: "",
+      randomMetaInstructions: "",
     },
   ]);
 
@@ -74,20 +68,21 @@ const RandomRecipe: FC = () => {
           });
           setRandomRecipe(randomRecipeArray);
         }
-        // setRandomRecipeDataMeta([
-        //   {
-        //     randomMetaTitle: data[0]["recipes"]["title"],
-        //     randomMetaSourceUrl: data[0]["recipes"]["sourceUrl"],
-        //     randomMetaReadyInMinutes: data[0]["recipes"]["readyInMinutes"],
-        //     randomMetaImage: data[0]["recipes"]["image"],
-        //     randomMetaSummary: data[0]["recipes"]["summary"],
-        //     randomMetaInstructions: data[0]["recipes"]["instructions"],
-        //   },
-        // ]);
+        setRandomRecipeDataMeta([
+          {
+            randomMetaTitle: data[0]["recipes"]["title"],
+            randomMetaSourceUrl: data[0]["recipes"]["sourceUrl"],
+            randomMetaReadyInMinutes: data[0]["recipes"]["readyInMinutes"],
+            randomMetaImage: data[0]["recipes"]["image"],
+            randomMetaSummary: data[0]["recipes"]["summary"],
+            randomMetaInstructions: data[0]["recipes"]["instructions"],
+          },
+        ]);
       })
       .catch(() => {
         console.error("error");
       });
+    console.log(randomRecipeDataMeta);
   };
 
   return (
