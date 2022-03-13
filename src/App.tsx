4366b0cd1a39539f40d5a0ea530e488a8818c869
matthewgstillman <matthewgstillman@gmail.com>
2022-03-13 13:56:11 -0600

    more modularization

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import React, { FC, FunctionComponent } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import RandomRecipe from "./Pages/RandomRecipe";
-import MealPlanner from "./Pages/MealPlanner";
-import AxiosRandomRecipe from "./Pages/AxiosRandomRecipe";
+import RandomRecipe from "./components/RandomRecipe";
+import MealPlanner from "./components/MealPlanner";
+import AxiosRandomRecipe from "./components/AxiosRandomRecipe";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export interface IApplicationProps {}
@@ -15,8 +13,6 @@ const App: FunctionComponent<IApplicationProps> = (props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/random" element={<RandomRecipe />} />
         <Route path="/mealplanner" element={<MealPlanner />} />
         <Route path="/axios" element={<AxiosRandomRecipe />} />
